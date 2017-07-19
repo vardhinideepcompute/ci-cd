50e00594de2e948789653dfa274a0c20d651eb1c
silpa <silpa@deepcompute.com>
2017-07-19 13:14:21 +0530

    Testing the addition program also

--- test_prime_no.py
+++ test_prime_no.py
@@ -1,5 +1,6 @@
 import unittest2
 from prime_no import is_prime
+from add import add
 
 print "Test file"
 class PrimesTestCase(unittest2.TestCase):
@@ -8,6 +9,9 @@ class PrimesTestCase(unittest2.TestCase):
     def test_is_five_prime(self):
         """Is five successfully determined to be prime?"""
         self.assertTrue(is_prime(5))
+class addTestCase(unittest2.TestCase):
+    def testadd(self):
+        self.assertEqual(add.add_num(5,5,5),15)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest2.main()
